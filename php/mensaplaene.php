bc3775086513f38006bf388572ef47dc7e8eabae
Christoph Friedrich <christoph@friedrich-whv.de>
2016-11-14 13:08:43 +0100

    Implemented new Mensaplan distribution method

--- php/mensaplaene.php
+++ php/mensaplaene.php
@@ -1,24 +1,45 @@
-<?php $url =  "http://www.studentenwerk-muenster.de/de/essen-a-trinken/mensen/mensa-am-ring"; 
-$input = @file_get_contents($url) or die('Could not access file: $url'); 
-$mensaplan;
-$tag = date("w");
-$tage = array("sonntag","montag","dienstag","mittwoch","donnerstag","freitag","samstag");
-
-	if( preg_match( '~<td id="'.$tage[$tag].'_menu1">\s*(.*?)\s*</td>~si', $input, $content ) ) 
-	{ 
-	    $mensaplan = "Menu 1: ".addslashes(trim( $content[1] ))."<br>";
-	}
-	if( preg_match( '~<td id="'.$tage[$tag].'_menu2">\s*(.*?)\s*</td>~si', $input, $content ) ) 
-	{ 
-	    $mensaplan = $mensaplan."Menu 2: ".addslashes(trim( $content[1] ))."<br>";
-	}
-	if( preg_match( '~<td id="'.$tage[$tag].'_menu3">\s*(.*?)\s*</td>~si', $input, $content ) ) 
-	{ 
-	    $mensaplan = $mensaplan."Menu 3: ".addslashes(trim( $content[1] ));
-	}
-    ?>
-	    <script>
-	    	addNews("Heute in der Mensa am Ring:", "<?php echo $mensaplan ?>");
-	    </script>
-	<?php
+<?php
+  $url = "http://speiseplan.stw-muenster.de/mensa_am_ring.xml";
+  /* 
+    Available data:
+    
+    mensa_aasee.xml
+    mensa_am_ring.xml
+    mensa_bispinghof.xml
+    mensa_da_vinci.xml
+    mensa_steinfurt.xml
+    bistro_coerdehof.xml
+    bistro_denkpause.xml
+    bistro_durchblick.xml
+    bistro_frieden.xml
+    bistro_huefferstift.xml
+    bistro_kabu.xml
+    bistro_katho.xml
+    bistro_oeconomicum.xml
+    
+    "Die Dateien werden Mo. - Sa. von 06:01 bis 16:01 Uhr alle 5 Minuten aktualisiert."
+    
+    The Studentenwerk outsourced this to digital signage company infomax.de
+  */
+   
+  $input = @file_get_contents($url) or die("Could not access file: $url"); 
+  $mensa = simplexml_load_string($input) or die("Could not parse XML to object");
+  
+  $mensaplan = '';
+	
+	// Go through all of today's meals
+  foreach($mensa->date[0]->item as $meal)
+	{
+    // Potentially remove "Heute am " from "Heute am Aktionsstand WOK" to make it shorter
+    $category = str_replace('Heute am ', '', $meal->category);
+    // Remove additives list from meal description (always in round brackets) and also remove
+    // possible newlines that would cause the JS to complain about an "unterminated string literal"
+    $name = preg_replace(['/ ?\([^(]*\)/', '/\s/'], ['', ' '], $meal->meal);
+    
+    $mensaplan .= "$category: $name<br>";
+  }
 ?>
+
+<script>
+	addNews("Heute in der Mensa am Ring:", "<?php echo addslashes($mensaplan); ?>");
+</script>
