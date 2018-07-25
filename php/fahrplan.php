6ca02634e3f317fd95fb90bf50648045ed04f042
Christoph Friedrich <christoph@friedrich-whv.de>
2018-07-25 17:34:18 +0200

    Fix #11

--- php/fahrplan.php
+++ php/fahrplan.php
@@ -1,8 +1,8 @@
 <?php
 echo "<div id='einwaerts'>";
-echo file_get_contents('http://www.stadtwerke-muenster.de/fis/ajaxrequest.php?mastnr=5992');
+echo file_get_contents('https://www.stadtwerke-muenster.de/fis/ajaxrequest.php?mastnr=5992');
 echo "</div><div id='auswaerts'>";
-echo file_get_contents('http://www.stadtwerke-muenster.de/fis/ajaxrequest.php?mastnr=5991');
+echo file_get_contents('https://www.stadtwerke-muenster.de/fis/ajaxrequest.php?mastnr=5991');
 echo "</div>";
 // echo "<br>";
 // echo file_get_contents('http://www.stadtwerke-muenster.de/fis/ajaxrequest.php?mastnr=5202');
